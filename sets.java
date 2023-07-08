2775576d49e422111b9261a167b75bed1b7a24a8
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-07-08 14:51:27 +0530

    Add files via upload

---
+++ sets.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class sets {
+    public static void main(String[] args) {
+        // HashSet<Integer>set = new HashSet<>();
+        // set.add(1);
+        // set.add(2);
+        // set.add(3);
+        // set.add(1);
+        // System.out.println(set);
+        // if(set.contains(7)){
+        // System.out.println("True");
+        // }
+        // else{
+        // System.out.println("False");
+        // }
+        // set.remove(2);
+        // System.out.println(set.size());
+        // set.clear();
+        // System.out.println(set.size());
+        // System.out.println(set.isEmpty());
+
+
+
+        // HashSet<String>cities = new HashSet<>();
+        // cities.add("Delhi");
+        // cities.add("Bengaluru");
+        // cities.add("kolkata");
+        // System.out.println(cities);
+        // Iterator it =cities.iterator();
+        // while(it.hasNext()){
+        // System.out.println(it.next());
+        // }
+        // System.out.println("------------------------------------");
+
+        // for(String city : cities){
+        // System.out.println(city);
+        // }
+
+
+
+        // LinkedHashSet<String>lhs = new LinkedHashSet<>();
+        // lhs.add("Delhi");
+        // lhs.add("Bengaluru");
+        // lhs.add("kolkata");
+        // System.out.println(lhs);
+        // lhs.remove("Delhi");
+        // System.out.println(lhs);
+
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Abhinav");
+        ts.add("AAkash");
+        ts.add("Arushi");
+        System.out.println(ts);
+    }
+}
